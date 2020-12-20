5ecfc418dac52153bd04b81dd6183d79e2389475
MarcellinhoMago <marcellinho.domingues@live.com>
2020-12-20 19:38:33 -0300

    VendedorServicos e basico FindAll

--- SalesWebMvc/Controllers/VendedoresController.cs
+++ SalesWebMvc/Controllers/VendedoresController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Servicos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,17 @@ namespace SalesWebMvc.Controllers
 {
     public class VendedoresController : Controller
     {
+        private readonly VendedorServicos _vendedorServicos;
+
+        public VendedoresController(VendedorServicos vendedorServicos)
+        {
+            _vendedorServicos = vendedorServicos;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _vendedorServicos.FindAll();
+            return View(list);
         }
     }
 }
---
+++ SalesWebMvc/Servicos/VendedorServicos.cs
@@ -0,0 +1,24 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Servicos
+{
+    public class VendedorServicos
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public VendedorServicos(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Vendedor> FindAll()
+        {
+            return _context.Vendedor.ToList();
+        }
+    }
+}
