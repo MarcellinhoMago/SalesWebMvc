9926df985d71652902c8d418ff9880f2d55eae3c
MarcellinhoMago <marcellinho.domingues@live.com>
2021-10-19 18:21:04 -0300

    Alterando as classes Department e SalesRecord para prtuguês do Brasil.

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections.Generic;
 using System;
 using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
+
+        [Display(Name = "Departamento")]
         public string Name { get; set; }
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -9,11 +9,15 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Data")]
         public DateTime Date { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Display(Name = "Valor")]
         public double Amount { get; set; }
         public SaleStatus Status { get; set; }
+
+        [Display(Name = "Vendedor")]
         public Seller Seller { get; set; }
 
         public SalesRecord()
