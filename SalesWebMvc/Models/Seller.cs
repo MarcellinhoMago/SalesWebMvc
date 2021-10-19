8942434e4330108043e647f027e9103bb3048cc0
MarcellinhoMago <marcellinho.domingues@live.com>
2021-10-19 18:11:42 -0300

    Alterando a classe Seller para portugês do Brasil

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,29 +9,32 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Display(Name = "Nome")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} o tamanho deve estar entre {2} and {1}")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
+        [Required(ErrorMessage = "{0} obrigatório")]
         [Display(Name = "E-mail")]
-        [EmailAddress(ErrorMessage = "Enter a valid e-mail")]
+        [EmailAddress(ErrorMessage = "Entre com um e-mail válido")]
         [DataType(DataType.EmailAddress)] 
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Display(Name = "Data Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
-        [Required(ErrorMessage = "{0} required")]
-        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
-        [Display(Name = "Base Salary")]
+        [Required(ErrorMessage = "{0} obrigatório")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} deve ser de {1} to {2}")]
+        [Display(Name = "Base Salário")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
 
+        [Display(Name = "Departamento")]
         public Department Department { get; set; }
+
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
@@ -63,6 +66,5 @@ namespace SalesWebMvc.Models
         {
             return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
         }
-
     }
 }
